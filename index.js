667bb524f573c3c4b15c278d60a8ec5bc4983c68
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2023-05-04 20:06:53 +0600

    serverside added

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const courses = require('./data.json')
+const chefs = require('./chefData.json')
+const port = 5000
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hey! Foodie Server Running')
+})
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+app.get('/chefs', (req, res) => {
+    res.send(chefs)
+})
+app.get('/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) res.status(404).send({})
+    res.send(course)
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
